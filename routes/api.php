4b0469e8e46d2a76f69bd788543cc9e8758e83dd
NgPhuong <phuongngusee@gmail.com>
2024-11-05 09:15:03 +0700

    refactor: clean code api.php, refactor Controllers/Auth

--- routes/api.php
+++ routes/api.php
@@ -11,36 +11,6 @@ use App\Http\Controllers\Api\V1\ProColorSizeController;
 use App\Http\Controllers\Api\V1\ProductImageController;
 use App\Http\Controllers\Api\V1\ProductOrderController;
 use App\Http\Controllers\Api\V1\TestController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-// Route::apiResource('size',SizeController::class);
-
-//api/v1
-Route::group(['prefix' => 'v1','namespace' => 'App\Http\Controllers\Api\V1'],function(){
-    //add route here
-    Route::apiResource('size',SizeController::class);
-    Route::apiResource('color',ColorController::class);
-    Route::apiResource('product',ProductController::class);
-    Route::apiResource('category',CategoryController::class);
-    Route::apiResource('categoryType',CategoryTypeController::class);
-    Route::apiResource('test',TestController::class);
-    Route::apiResource('proColorSize',ProColorSizeController::class);
-    Route::apiResource('productImage',ProductImageController::class);
-    Route::apiResource('order',OrderController::class);
-    Route::apiResource('bill',BillController::class);
-    // Route::get('productOrder/{field1}/{field2}', [ProductOrderController::class, 'show']);
-    
-});
-
-<?php
-
-use App\Http\Controllers\Api\V1\SizeController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -48,18 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
 // Route::apiResource('size',SizeController::class);
 
 //api/v1
-Route::group(['prefix' => 'v1','namespace' => 'App\Http\Controllers\Api\V1'],function(){
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
     //add route here
-    Route::apiResource('size',SizeController::class);
+    Route::apiResource('size', SizeController::class);
+    Route::apiResource('color', ColorController::class);
+    Route::apiResource('product', ProductController::class);
+    Route::apiResource('category', CategoryController::class);
+    Route::apiResource('categoryType', CategoryTypeController::class);
+    Route::apiResource('test', TestController::class);
+    Route::apiResource('proColorSize', ProColorSizeController::class);
+    Route::apiResource('productImage', ProductImageController::class);
+    Route::apiResource('order', OrderController::class);
+    Route::apiResource('bill', BillController::class);
+    // Route::get('productOrder/{field1}/{field2}', [ProductOrderController::class, 'show']);
 });
 
-Route::post('/admin/auth/register',[AuthController::class, 'register']);
-Route::post('/admin/auth/login',[AuthController::class, 'login']);
-
-
-Route::post('/admin/auth/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
+//auth
+Route::post('/admin/auth/register', [AuthController::class, 'register']);
+Route::post('/admin/auth/login', [AuthController::class, 'login']);
+Route::post('/admin/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
