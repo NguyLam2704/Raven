46f9baf63bde5701d6ebd9deff1fca7d2f91f868
LuanNe <134901494+mluan204@users.noreply.github.com>
2024-11-09 11:45:13 +0700

    fix(ui): fix path in App.jsx and index.jsx (#14)
    fix(ui): fix path in App.jsx and index.jsx

--- resources/js/admin/components/App.jsx
+++ resources/js/admin/components/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import Router from "../router/index";
-import HomeUser from "../user/pages/HomeUser";
-import Category from "../user/pages/Category";
+
 
 const App = () => {
     return(
